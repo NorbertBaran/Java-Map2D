c4a5d99c3d3e6ddf14e510a288d0b87fb12def77
norbertbaran <norbertbaran1@gmail.com>
2019-11-29 01:27:17 +0100

    Delete debug System.out. Change object to template

--- src/main/java/uj/java/pwj2019/map2d/MyMap2D.java
+++ src/main/java/uj/java/pwj2019/map2d/MyMap2D.java
@@ -3,35 +3,35 @@ package uj.java.pwj2019.map2d;
 import java.util.*;
 import java.util.function.Function;
 
-public class MyMap2D implements Map2D {
-    Map<Object, Map<Object, Object>> map2D=new LinkedHashMap<>();
+public class MyMap2D<R, C, V> implements Map2D<R, C, V> {
+    Map<R, Map<C, V>> map2D=new LinkedHashMap<>();
 
     @Override
-    public Object put(Object rowKey, Object columnKey, Object value) {
+    public V put(R rowKey, C columnKey, V value) {
         if(rowKey==null || columnKey==null)
             throw new NullPointerException();
         if(!map2D.containsKey(rowKey)) {
             map2D.put(rowKey, new HashMap<>());
         }
-        Object preValue=map2D.get(rowKey).get(columnKey);
+        V preValue=map2D.get(rowKey).get(columnKey);
         map2D.get(rowKey).put(columnKey, value);
         return preValue;
     }
 
     @Override
-    public Object get(Object rowKey, Object columnKey) {
+    public V get(R rowKey, C columnKey) {
         if(map2D.get(rowKey)!=null)
             return map2D.get(rowKey).get(columnKey);
         return null;
     }
 
     @Override
-    public Object getOrDefault(Object rowKey, Object columnKey, Object defaultValue) {
+    public V getOrDefault(R rowKey, C columnKey, V defaultValue) {
         return map2D.getOrDefault(rowKey, Collections.emptyMap()).getOrDefault(columnKey, defaultValue);
     }
 
     @Override
-    public Object remove(Object rowKey, Object columnKey) {
+    public V remove(R rowKey, C columnKey) {
         return map2D.getOrDefault(rowKey, Collections.emptyMap()).remove(columnKey);
     }
 
@@ -48,11 +48,9 @@ public class MyMap2D implements Map2D {
     @Override
     public int size() {
         int size=0;
-        Set<Map.Entry<Object, Map<Object, Object>>> map2dSet=map2D.entrySet();
-        for(Map.Entry<Object, Map<Object, Object>> map: map2dSet){
+        Set<Map.Entry<R, Map<C, V>>> map2dSet=map2D.entrySet();
+        for(Map.Entry<R, Map<C, V>> map: map2dSet){
             size+=map.getValue().size();
-            System.out.println("Test2");
-            System.out.println(map);
         }
         return size;
     }
@@ -63,67 +61,67 @@ public class MyMap2D implements Map2D {
     }
 
     @Override
-    public Map rowView(Object rowKey) {
+    public Map<C, V> rowView(R rowKey) {
         return map2D.get(rowKey);
     }
 
     @Override
-    public Map columnView(Object columnKey) {
-        Map<Object, Object> map=new HashMap<Object, Object>();
-        Set<Map.Entry<Object, Map<Object, Object>>> map2dSet=map2D.entrySet();
-        for(Map.Entry<Object, Map<Object, Object>> iMap: map2dSet)
+    public Map<R, V> columnView(C columnKey) {
+        Map<R, V> map=new HashMap<R, V>();
+        Set<Map.Entry<R, Map<C, V>>> map2dSet=map2D.entrySet();
+        for(Map.Entry<R, Map<C, V>> iMap: map2dSet)
             if(iMap.getValue().get(columnKey)!=null)
                 map.put(iMap.getKey(), iMap.getValue().get(columnKey));
         return map;
     }
 
     @Override
-    public boolean hasValue(Object value) {
-        Set<Map.Entry<Object, Map<Object, Object>>> map2dSet=map2D.entrySet();
-        for(Map.Entry<Object, Map<Object, Object>> iMap: map2dSet)
+    public boolean hasValue(V value) {
+        Set<Map.Entry<R, Map<C, V>>> map2dSet=map2D.entrySet();
+        for(Map.Entry<R, Map<C, V>> iMap: map2dSet)
             if(iMap.getValue().containsValue(value))
                 return true;
         return false;
     }
 
     @Override
-    public boolean hasKey(Object rowKey, Object columnKey) {
+    public boolean hasKey(R rowKey, C columnKey) {
         if(map2D.get(rowKey)!=null)
             return map2D.get(rowKey).get(columnKey)!=null;
         return false;
     }
 
     @Override
-    public boolean hasRow(Object rowKey) {
+    public boolean hasRow(R rowKey) {
         return map2D.containsKey(rowKey);
     }
 
     @Override
-    public boolean hasColumn(Object columnKey) {
-        Set<Map.Entry<Object, Map<Object, Object>>> map2dSet=map2D.entrySet();
-        for(Map.Entry<Object, Map<Object, Object>> iMap: map2dSet)
+    public boolean hasColumn(C columnKey) {
+        Set<Map.Entry<R, Map<C, V>>> map2dSet=map2D.entrySet();
+        for(Map.Entry<R, Map<C, V>> iMap: map2dSet)
             if(iMap.getValue().containsKey(columnKey))
                 return true;
         return false;
     }
 
     @Override
-    public Map rowMapView() {
-        Map<Object, Map<Object, Object>> result=new LinkedHashMap<>();
-        Set<Map.Entry<Object, Map<Object, Object>>> map2dSet=map2D.entrySet();
-        for(Map.Entry<Object, Map<Object, Object>> iMap2D: map2dSet)
+    public Map<R, Map<C,V>> rowMapView() {
+        Map<R, Map<C, V>> result=new LinkedHashMap<>();
+        Set<Map.Entry<R, Map<C, V>>> map2dSet=map2D.entrySet();
+        for(Map.Entry<R, Map<C, V>> iMap2D: map2dSet)
             result.put(iMap2D.getKey(), Collections.unmodifiableMap(Map.copyOf(iMap2D.getValue())));
         return Collections.unmodifiableMap(Map.copyOf(result));
     }
 
     @Override
-    public Map columnMapView() {
+    public Map<C, Map<R,V>> columnMapView() {
         Map2D columnMap=Map2D.createInstance();
-        Set<Map.Entry<Object, Map<Object, Object>>> map2dSet=map2D.entrySet();
-        for(Map.Entry<Object, Map<Object, Object>> iMap: map2dSet){
+        Set<Map.Entry<R, Map<C, V>>> map2dSet=map2D.entrySet();
+        for(Map.Entry<R, Map<C, V>> iMap: map2dSet){
             Object rowKey=iMap.getKey();
-            Set<Map.Entry<Object, Object>> mapSet=iMap.getValue().entrySet();
-            for(Map.Entry<Object, Object> element: mapSet){
+            Set<Map.Entry<C, V>> mapSet=iMap.getValue().entrySet();
+            for(Map.Entry<C, V> element: mapSet){
                 Object columnKey=element.getKey();
                 Object value=element.getValue();
 
@@ -134,7 +132,7 @@ public class MyMap2D implements Map2D {
     }
 
     @Override
-    public Map2D fillMapFromRow(Map target, Object rowKey) {
+    public Map2D<R, C, V> fillMapFromRow(Map<? super C, ? super V> target, R rowKey) {
         if(map2D.containsKey(rowKey)) {
             target.putAll(map2D.get(rowKey));
         }
@@ -142,9 +140,9 @@ public class MyMap2D implements Map2D {
     }
 
     @Override
-    public Map2D fillMapFromColumn(Map target, Object columnKey) {
-        Set<Map.Entry<Object, Map<Object, Object>>> map2dSet=map2D.entrySet();
-        for(Map.Entry<Object, Map<Object, Object>> iMap: map2dSet){
+    public Map2D<R, C, V> fillMapFromColumn(Map<? super R, ? super V> target, C columnKey) {
+        Set<Map.Entry<R, Map<C, V>>> map2dSet=map2D.entrySet();
+        for(Map.Entry<R, Map<C, V>> iMap: map2dSet){
             if(iMap.getValue().get(columnKey)!=null)
                 target.put(iMap.getKey(), iMap.getValue().get(columnKey));
         }
@@ -152,45 +150,49 @@ public class MyMap2D implements Map2D {
     }
 
     @Override
-    public Map2D putAll(Map2D source) {
-        this.map2D=source.rowMapView();
+    public Map2D<R, C, V>  putAll(Map2D<? extends R, ? extends C, ? extends V> source) {
+        Map2D map2D=source;
+        this.map2D=map2D.rowMapView();
         return this;
     }
 
     @Override
-    public Map2D putAllToRow(Map source, Object rowKey) {
-        Set<Map.Entry<Object, Object>> mapSet=source.entrySet();
-        for(Map.Entry<Object, Object> element: mapSet){
+    public Map2D<R, C, V>  putAllToRow(Map<? extends C, ? extends V> source, R rowKey) {
+        Set<? extends Map.Entry<? extends C, ? extends V>> mapSet=source.entrySet();
+        for(Map.Entry<? extends C, ? extends V> element: mapSet){
             this.put(rowKey, element.getKey(), element.getValue());
         }
         return this;
     }
 
     @Override
-    public Map2D putAllToColumn(Map source, Object columnKey) {
-        Set<Map.Entry<Object, Object>> mapSet=source.entrySet();
-        for(Map.Entry<Object, Object> element: mapSet){
+    public Map2D<R, C, V>  putAllToColumn(Map<? extends R, ? extends V> source, C columnKey) {
+        Set<? extends Map.Entry<? extends R, ? extends V>> mapSet=source.entrySet();
+        for(Map.Entry<? extends R, ? extends V> element: mapSet){
             this.put(element.getKey(), columnKey, element.getValue());
         }
         return this;
     }
 
     @Override
-    public Map2D copyWithConversion(Function rowFunction, Function columnFunction, Function valueFunction) {
+    public <R2, C2, V2> Map2D<R2, C2, V2> copyWithConversion(
+            Function<? super R, ? extends R2> rowFunction,
+            Function<? super C, ? extends C2> columnFunction,
+            Function<? super V, ? extends V2> valueFunction) {
         Map2D convertedMap2D=Map2D.createInstance();
 
-        Set<Map.Entry<Object, Map<Object, Object>>> map2dSet=map2D.entrySet();
-        for(Map.Entry<Object, Map<Object, Object>> iMap: map2dSet){
-            Object rowKey=iMap.getKey();
-            Object convertedRowKey=rowFunction.apply(rowKey);
+        Set<Map.Entry<R, Map<C, V>>> map2dSet=map2D.entrySet();
+        for(Map.Entry<R, Map<C, V>> iMap: map2dSet){
+            R rowKey=iMap.getKey();
+            R2 convertedRowKey=rowFunction.apply(rowKey);
 
-            Set<Map.Entry<Object, Object>> mapSet=iMap.getValue().entrySet();
-            for(Map.Entry<Object, Object> element: mapSet){
-                Object columnKey=element.getKey();
-                Object convertedColumnKey=columnFunction.apply(columnKey);
+            Set<Map.Entry<C, V>> mapSet=iMap.getValue().entrySet();
+            for(Map.Entry<C, V> element: mapSet){
+                C columnKey=element.getKey();
+                C2 convertedColumnKey=columnFunction.apply(columnKey);
 
-                Object value=element.getValue();
-                Object convertedValue=valueFunction.apply(value);
+                V value=element.getValue();
+                V2 convertedValue=valueFunction.apply(value);
 
                 convertedMap2D.put(convertedRowKey, convertedColumnKey, convertedValue);
             }
